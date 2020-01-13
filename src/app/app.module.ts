11be1200caef566f20a48d63fd89a5d10e479cd1
Ong <Ong@gmail.com>
2020-01-14 01:02:07 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,36 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule,Apollo } from 'apollo-angular';
+import { HttpLinkModule,HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+import { ListComponent } from './components/list/list.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(
+    apollo:Apollo,
+    httpLink:HttpLink
+  ){
+    apollo.create({
+      link:httpLink.create({ uri: 'https://vm8mjvrnv3.lp.gql.zone/graphql'}),
+      cache: new InMemoryCache()
+    })
+  }
+}
